a197daa312b9c16cca6aa32996e06c2430d56d8f
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-04-13 01:38:54 +0530

    top sales section

---
+++ src/components/FlashSales.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import rightArrowBlack from '../assets/rightArrowBlack.png'
+import leftArrowBlack from '../assets/leftArrowBlack.png'
+const FlashSales = () => {
+  return (
+    <div className='w-full mt-16'>
+      <div className='max-w-screen-xl'>
+        {/* top header  */}
+        <header className='flex items-center gap-[16px]'>
+            <div className='w-[20px] h-[40px] bg-[#DB4444] rounded-md'></div>
+            <p className='text-[#DB4444] font-poppins font-semibold text-[16px]'>Today's</p>
+        </header>
+        {/* mini header  */}
+        <header className='flex items-end justify-between'>
+            {/* heading  */}
+            <p className='font-inter text-[36px] font-semibold leading-7'>Flash Sales</p>
+            {/* timer */}
+            <div className='flex items-center w-[302px] justify-between mr-[20rem]'>
+                <div className='leading-7'>
+                    <p className='text-[12px] font-medium font-poppins'>Days</p>
+                    <p className='text-[32px] font-inter font-bold'>03</p>
+                </div>
+                <p className='text-[#DB4444] font-bold text-[1.3rem]'>:</p>
+                <div className='leading-7'>
+                    <p className='text-[12px] font-medium font-poppins'>Hours</p>
+                    <p className='text-[32px] font-inter font-bold'>23</p>
+                </div>
+                <p className='text-[#DB4444] font-bold text-[1.3rem]'>:</p>
+                <div className='leading-7'>
+                    <p className='text-[12px] font-medium font-poppins'>Minutes</p>
+                    <p className='text-[32px] font-inter font-bold'>19</p>
+                </div>
+                <p className='text-[#DB4444] font-bold text-[1.3rem]'>:</p>
+                <div className='leading-7'>
+                    <p className='text-[12px] font-medium font-poppins'>Seconds</p>
+                    <p className='text-[32px] font-inter font-bold'>56</p>
+                </div>
+            </div>
+            {/* direction */}
+            <div className='flex gap-1 items-center'>
+                <button className='w-9'><img src={leftArrowBlack} alt="" /></button>
+                <button className='w-9'><img src={rightArrowBlack} alt="" /></button>
+            </div>
+        </header>
+      </div>
+    </div>
+  )
+}
+
+export default FlashSales
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Categories from './Categories'
-import Banner from './Banner'
+import React from "react";
+import Categories from "./Categories";
+import Banner from "./Banner";
+import FlashSales from "./FlashSales";
 const Home = () => {
   return (
-    <div className='w-full'>
-      <div className='max-w-screen-xl m-auto flex justify-between'>
-      <Categories/>
-      <Banner/>
+    <div className="w-full">
+      <div className="max-w-screen-xl m-auto">
+        <div className="flex justify-between">
+          <Categories />
+          <Banner />
+        </div>
+        <FlashSales/>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
