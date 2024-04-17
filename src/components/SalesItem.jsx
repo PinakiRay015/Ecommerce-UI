e2cae36db0ad6b2356a25bd58cc0fd8efcc3eb03
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-04-18 00:29:23 +0530

    best selling items

--- src/components/SalesItem.jsx
+++ src/components/SalesItem.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import wishlist from "../assets/wishlist.png";
 import Ratings from "./Ratings";
 import items from "./Cards/SalesCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
 const SalesItem = () => {
+  const [hoveredCardIndex, setHoveredCardIndex] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoveredCardIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredCardIndex(null);
+  };
+
   var settings = {
     dots: false,
     infinite: false,
@@ -18,27 +29,31 @@ const SalesItem = () => {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 615,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
+
   return (
     <div className="">
       <Slider className="" {...settings}>
-        {items.map((e) => {
+        {items.map((e, index) => {
           return (
-            <div className="flex px-2">
+            <div className="flex px-2" key={index}>
               <div className="h-fit">
-                {/* item image */}
-                <div className="relative h-[250px] bg-[#F5F5F5] grid place-content-center">
+                <div
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                  className={`relative h-[250px] grid place-content-center bg-[#F5F5F5]`}
+                >
                   <nav className="flex justify-between absolute w-full top-0 px-1 py-1">
                     <p className="bg-[#DB4444] px-3 rounded-lg text-white w-fit font-extralight text-[14px] flex justify-center items-center">
                       {e.discount}
@@ -50,17 +65,20 @@ const SalesItem = () => {
                     />
                   </nav>
                   <img className="w-[8rem]" src={e.img} alt="" />
-                  <footer className="h-[41px] flex items-center justify-center bg-black bottom-0 absolute w-full">
-                    <p className="text-white text-[16px] font-poppins">Add to cart</p>
+                  <footer
+                    className={`absolute ${
+                      hoveredCardIndex === index
+                        ? "h-[41px] text-white"
+                        : "h-[0px] text-[#F5F5F5]"
+                    } bottom-0 flex items-center justify-center bg-black w-full transition-all duration-200 ease-in-out`}
+                  >
+                    <p className=" text-[16px] font-poppins">Add to cart</p>
                   </footer>
                 </div>
-                {/* item description */}
                 <div className="leading-7 mt-2">
-                  {/* name */}
                   <p className="font-poppins font-medium text-[16px]">
                     {e.name}
                   </p>
-                  {/* price */}
                   <p className="flex gap-2">
                     <span className="font-medium text-[#DB4444] text-[16px]">
                       {e.newprice}
